8fcc84563f4afd65492be6e22592f19d7e747de8
Antonio Neil <bossneil20@gmail.com>
2023-04-09 04:22:47 -0500

    MovieForm class added

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,9 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, FileField
+from wtforms.validators import InputRequired
+from flask_wtf.file import FileAllowed, FileRequired
+
+class MovieForm(FlaskForm):
+    title = StringField('Title: ', validators=[InputRequired()])
+    description = TextAreaField('Description: ', validators=[InputRequired()])
+    poster = FileField('Upload Image of Poster: ', validators=[FileRequired(), FileAllowed('jpg', 'png')] )
